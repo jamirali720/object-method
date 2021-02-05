5884ba700f1ace5cf444c524af871e7e24ad9e4c
jamir ali <jamirali720@gmail.com>
2021-02-05 11:47:19 +0600

    added bind method

--- callApply.js
+++ callApply.js
@@ -13,3 +13,43 @@ const normalPerson = {
 normalPerson.chargeBill(200);
 normalPerson.chargeBill(5000)
 console.log(normalPerson.salary);
+
+
+
+
+const adamAli = {
+    name: 'adam ali',
+    id: 121,
+    phone : 01814245427,
+    salary : 15000,
+
+    billCharge : function(){
+        var vat = this.salary * 0.15;
+        this.salary = this.salary - vat;
+      
+       
+    }
+}
+adamAli.billCharge()
+console.log(adamAli.salary)
+
+
+
+
+
+const heroPerson  = {
+    firstName : 'Jamir ',
+    lastName : 'Ali',
+    salary : 25000, 
+}
+
+
+const heroMan1  =  adamAli.billCharge.bind(heroPerson);
+heroMan1()
+console.log(heroPerson.salary)
+
+
+
+const heroMan = normalPerson.chargeBill.bind(heroPerson);
+heroMan(5000)
+console.log(heroPerson.salary)
