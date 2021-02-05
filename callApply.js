6900127969bdbb10de2c0bec188fddf803c5ab1f
jamir ali <jamirali720@gmail.com>
2021-02-05 12:51:51 +0600

    added object method

--- callApply.js
+++ callApply.js
@@ -5,14 +5,14 @@ const normalPerson = {
     getFullName : function(){
         console.log(this.firstName, this.lastName)
     },
-    chargeBill : function(amount){
-        this.salary = this.salary- amount;
+    chargeBill : function(amount, tips, tax){
+        this.salary = this.salary- amount - tips - tax;
         return this.salary;
     }
 }
-normalPerson.chargeBill(200);
-normalPerson.chargeBill(5000)
-console.log(normalPerson.salary);
+// normalPerson.chargeBill(200);
+// normalPerson.chargeBill(5000)
+// console.log(normalPerson.salary);
 
 
 
@@ -24,14 +24,14 @@ const adamAli = {
     salary : 15000,
 
     billCharge : function(){
-        var vat = this.salary * 0.15;
+        var vat = this.salary * 0.15;           // vat is to inserted into  function not out of the function;
         this.salary = this.salary - vat;
       
        
     }
 }
-adamAli.billCharge()
-console.log(adamAli.salary)
+// adamAli.billCharge()
+// console.log(adamAli.salary)
 
 
 
@@ -44,12 +44,32 @@ const heroPerson  = {
 }
 
 
-const heroMan1  =  adamAli.billCharge.bind(heroPerson);
-heroMan1()
-console.log(heroPerson.salary)
+// const heroMan1  =  adamAli.billCharge.bind(heroPerson);
+// heroMan1()
+// console.log(heroPerson.salary)
 
 
 
-const heroMan = normalPerson.chargeBill.bind(heroPerson);
-heroMan(5000)
-console.log(heroPerson.salary)
+// const heroMan = normalPerson.chargeBill.bind(heroPerson);
+// heroMan(5000)
+// console.log(heroPerson.salary)
+
+const hubbyPerson  = {
+    firstName : 'samsul ', 
+    lastName : 'alam',
+    salary : 30000,
+}
+ normalPerson.chargeBill.call(hubbyPerson, 5000, 500, 50)  // this is call method   with more than one arguments;
+ console.log(hubbyPerson.salary)
+
+ adamAli.billCharge.call(y=hubbyPerson)
+ console.log(hubbyPerson.salary)
+
+ const happyPerson = {
+     firstName : 'ashraful ',
+     lastName : 'Islam', 
+     salary : 15000,
+ }
+
+ normalPerson.chargeBill.apply(happyPerson,[5000, 500, 50])   // this is apply method  define with [] after first argument;
+ console.log(happyPerson.salary)
